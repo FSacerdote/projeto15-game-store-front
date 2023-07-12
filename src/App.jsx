14ce866726bc8864c62dca25c15275aebf848845
anajbsouza <anaju.batsouza@gmail.com>
2023-07-12 19:20:58 -0300

    feat: layout das páginas de login e cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import SignInPage from "./pages/SignInPage"
+import SignUpPage from "./pages/SignUpPage"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/"/>
-      </Routes>
-    </BrowserRouter>
+    <PagesContainer>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<SignInPage />} />
+          <Route path="/cadastro" element={<SignUpPage />} />
+        </Routes>
+      </BrowserRouter>
+    </PagesContainer>
   )
 }
 
-export default App
+export default App;
+
+const PagesContainer = styled.main`
+  background-color: #8c11be;
+  width: calc(100vw - 50px);
+  max-height: 100vh;
+  padding: 25px;
+`
---
+++ src/pages/SignInPage.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+
+export default function SignInPage() {
+    return (
+        <SingInContainer>
+            <form>
+                <MyWalletLogo />
+                <input placeholder="E-mail" type="email" />
+                <input placeholder="Senha" type="password" autocomplete="new-password" />
+                <button>Entrar</button>
+            </form>
+
+            <Link to="/cadastro">
+                Primeira vez? Cadastre-se!
+            </Link>
+        </SingInContainer>
+    )
+}
+
+const SingInContainer = styled.section`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
